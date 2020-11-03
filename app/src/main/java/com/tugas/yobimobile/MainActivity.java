2a7a1fce9348919b246394f51659987da0d3efa0
Safania <safania_sitepu@yahoo.com>
2020-11-03 10:59:55 +0700

    Initial commit

---
+++ app/src/main/java/com/tugas/yobimobile/MainActivity.java
@@ -0,0 +1,31 @@
+package com.tugas.yobimobile;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_page);
+    }
+    public void register(View view) {
+//        Fungsi untuk mentrigger activity baru
+        Intent intent = new Intent(this, RegisterActivity.class);
+
+        startActivity(intent);
+
+    }
+}
